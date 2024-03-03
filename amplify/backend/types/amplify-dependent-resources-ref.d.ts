96c3daae023e50e25765c1de07706d141b736fa5
Ryan McFall <mcfall@hope.edu>
2024-03-03 08:28:16 -0500

    Initial files after flutter create, amplify init, and amplify add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cognitoauth626dcdde": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/cognitoauth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cognitoauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

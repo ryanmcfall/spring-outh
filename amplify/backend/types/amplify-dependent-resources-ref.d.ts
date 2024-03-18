37fc8283acc80d54711040639723afc26360cd0f
Ryan McFall <mcfall@hope.edu>
2024-03-18 07:40:12 -0400

    Use amplify import / amplify push to configure Amplify to use CDK-created user pool

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "cognitoauth7150f017": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
